ba3401b6fd717a483eefda932fd949ac9e62c06a
mlbarry <mlbarry63@yahoo.com>
2015-11-23 18:22:45 -0500

    Changes to JS

--- final project/js/main.js
+++ final project/js/main.js
@@ -1,20 +1,21 @@
-var clickOne=0;
-var clickTwo=0;
-var clickThree=0;
+var clickOne = 0;
+var clickTwo = 0;
+var clickThree = 0;
+
 $('#button-one').click(function(){
-   clickOne +=1;
+   clickOne += 1;
   
-  if (clickOne=1){
+  if (clickOne == 1){
     console.log(clickOne);
     $('#button-one').css('background-color','yellow');
   }
-  else if (clickOne=2){
+  else if (clickOne == 2){
     console.log(clickOne);
-      $('#button-one').css('background-color','gree');
-  } else if (clickOne=3){
+      $('#button-one').css('background-color','green');
+  } else if (clickOne == 3){
     $('#button-one').css();
     
-  } else if (clickOne=4) {
+  } else if (clickOne == 4) { //reset the button 
      $('#button-one').css(grey);
     clickOne = 0;
   }
@@ -29,3 +30,12 @@ $('#button-two').click(function(){
 $('#button-three').click(function(){
   
 });
+
+// Click on main photo, show information
+
+$(#img-id).click(function(){
+  $(#text-id).toggleClass('show-text'); //create class in css with property:value = display: block;
+});
+
+//
+
